c592a2d7a2884c0d120c2e39090a624e0b59334a
Said <104688591+dias090@users.noreply.github.com>
2023-12-09 16:55:27 +0500

    my_mastermind

--- my_mastermind.c
+++ my_mastermind.c
@@ -4,51 +4,51 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#define kodning_uzunligi 4
-void tekshirish_funksiyasi(char* taxmin, char* maxviy_kod, int* tugri, int* no_tugri)
+#define the_length_of_secret_code 4
+void checking_function(char* users_guess, char* secret_code, int* right, int* wrong)
 {
-    int i = 0; *tugri=0,*no_tugri=0;   
-    int taxmin_taxmin[10] = {0};
+    int i = 0; *right=0,*wrong=0;   
+    int users_guess_users_guess[10] = {0};
     int c1[10] = {0};
-    for (int i = 0; i < kodning_uzunligi; i++){
-        if (taxmin[i] == maxviy_kod[i]){
-            (*tugri)++;
+    for (int i = 0; i < the_length_of_secret_code; i++){
+        if (users_guess[i] == secret_code[i]){
+            (*right)++;
         } else {
-            taxmin_taxmin[taxmin[i] - '0']++;
-            c1[maxviy_kod[i] - '0']++;
+            users_guess_users_guess[users_guess[i] - '0']++;
+            c1[secret_code[i] - '0']++;
         }
     }
     while(i<10){
-        if (c1[i] < taxmin_taxmin[i]){
-            *no_tugri += c1[i];
+        if (c1[i] < users_guess_users_guess[i]){
+            *wrong += c1[i];
         } else {
-            *no_tugri += taxmin_taxmin[i];
+            *wrong += users_guess_users_guess[i];
         }
         i++;
     }
 }
 
-char* Randomiser(char* random_kod)
+char* Randomiser(char* random_code)
 {
-    const char raqamlar[] = "0123456789";
+    const char numbers[] = "0123456789";
     int a1 = 0;
-    while(a1<kodning_uzunligi){
-        random_kod[a1] = raqamlar[rand() % 10];
+    while(a1<the_length_of_secret_code){
+        random_code[a1] = numbers[rand() % 10];
         a1++;
     }
-    random_kod[kodning_uzunligi] = '\0';
-    return random_kod;
+    random_code[the_length_of_secret_code] = '\0';
+    return random_code;
 }
 
-int kiritilgan_kodni_tekshirish_funksiyasi(char* kiritilgan_raqam)
+int input_code_check_function(char* input_code)
 {
     int i = 0;
-    if (strlen(kiritilgan_raqam) != kodning_uzunligi){
+    if (strlen(input_code) != the_length_of_secret_code){
         return 0;
     }
     
-    while(i<kodning_uzunligi){
-        if (kiritilgan_raqam[i] < '0' || kiritilgan_raqam[i] > '9'){
+    while(i<the_length_of_secret_code){
+        if (input_code[i] < '0' || input_code[i] > '9'){
             return 0;
         }
         i++;
@@ -56,43 +56,45 @@ int kiritilgan_kodni_tekshirish_funksiyasi(char* kiritilgan_raqam)
     return 1;
 }
 
+
+
 int main(int argc, char** argv)
 {
-    char* maxviy_kod = calloc(sizeof(char), kodning_uzunligi + 1);
+    char* the_secret_code = calloc(sizeof(char), the_length_of_secret_code + 1);
     int maksimal_urinishlar = 10;
     for (int i = 1; i < argc; i++){
         if (strcmp(argv[i], "-c") == 0 && i + 1 < argc){
-            strncpy(maxviy_kod, argv[i + 1], kodning_uzunligi);
-            maxviy_kod[kodning_uzunligi] = '\0';
+            strncpy(the_secret_code, argv[i + 1], the_length_of_secret_code);
+            the_secret_code[the_length_of_secret_code] = '\0';
         }else if (strcmp(argv[i], "-t") == 0 && i + 1 < argc){
             maksimal_urinishlar = atoi(argv[i + 1]);
         }
     }
-    if (strlen(maxviy_kod) == 0){
+    if (strlen(the_secret_code) == 0){
         srand(time(NULL));
-        Randomiser(maxviy_kod);
+        Randomiser(the_secret_code);
     }
-    char foydalanuvchi_taxmini[kodning_uzunligi + 1];
-    int tugri, no_tugri;
+    char users_guess[the_length_of_secret_code + 1];
+    int right, wrong;
     int rounds = 0;
     printf("Will you find the secret code?\nPlease enter a valid guess\n");
     while (rounds < maksimal_urinishlar){
         printf("---\nRound %d\n>", rounds);
-        if (read(0, foydalanuvchi_taxmini, kodning_uzunligi + 1) == -1){
+        if (read(0, users_guess, the_length_of_secret_code + 1) == -1){
             printf("Error reading input.\n");
             return 1;
         }
-        foydalanuvchi_taxmini[kodning_uzunligi] = '\0';
-        if (!kiritilgan_kodni_tekshirish_funksiyasi(foydalanuvchi_taxmini)){
+        users_guess[the_length_of_secret_code] = '\0';
+        if (!input_code_check_function(users_guess)){
             printf("Wrong input!\n");
             continue;
         }
-        tekshirish_funksiyasi(foydalanuvchi_taxmini, maxviy_kod, &tugri, &no_tugri);
-        if (tugri == kodning_uzunligi){
+        checking_function(users_guess, the_secret_code, &right, &wrong);
+        if (right == the_length_of_secret_code){
             printf("Congratz! You did it!\n");
             return 0;
         }
-        printf("Well placed pieces: %d\nMisplaced pieces: %d\n", tugri, no_tugri);
+        printf("Well placed pieces: %d\nMisplaced pieces: %d\n", right, wrong);
         rounds++;
     }
     printf("---\nGame over. You didn't find the secret code.\n");

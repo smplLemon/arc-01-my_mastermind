2886d87fba362a07ec8de7e43b20f8660b6d48d7
Burxonov Alimardon <155613397+alimardonb@users.noreply.github.com>
2024-02-07 11:04:07 +0500

    Update my_mastermind.c

--- my_mastermind.c
+++ my_mastermind.c
@@ -7,6 +7,7 @@
  #define CODE_LENGTH 4
  #define MAX_ATTEMPTS 10
  void generateSecretCode(char* secretCode);
+
  void evaluateGuess(const char* secretCode, const char* guess, int* wellPlaced, int* misplaced);
  int isValidGuess(const char* guess);
 

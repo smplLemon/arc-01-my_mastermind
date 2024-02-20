c34ca144504612843e6b8a2cae3fc1eb8bedebd0
Panda17uz <140608719+Panda17uz@users.noreply.github.com>
2024-02-20 17:56:45 +0500

    Numonov_Gofuramin_finished

---
+++ my_mc/README.md
@@ -0,0 +1,29 @@
+## Welcome to My Mastermind
+Numonov_Gofuramin 
+
+
+## Task
+What is the problem? And where is the challenge
+NO PROBLEM
+
+## Description
+Mastermind is a game composed of 9 pieces of different colors.
+A secret code is then composed of 4 distinct pieces.
+
+The player has 10 attempts to find the secret code.
+After each input, the game indicates to the player the number of well placed pieces and the number of misplaced pieces.
+
+Pieces will be '0' '1' '2' '3' '4' '5' '6' '7' '8'.
+
+If the player finds the code, he wins, and the game stops.
+A misplaced piece is a piece that is present in the secret code butthat is not in a good position.
+
+You must read the player's input from the standard input.
+
+Your program will also receive the following parameters:
+## Installation
+ow to install your project? npm install? make? make re?
+npm i, npm start
+## Usage
+How does it work?
+React npm i
---
+++ my_mc/my_mastermind.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+char *generateRandomCode() {
+    srand(time(NULL));
+    char* code = (char *)malloc(sizeof(char) * 5);
+    
+    for (int i = 0; i < 4; i++) {
+        code[i] = rand() % 9 + '0';
+        
+        for (int j = 0; j < i; j++) {    
+            if (code[i] == code[j]) {
+                i--;
+                break;
+            }
+        }
+    }
+    
+    code[4] = '\0';
+    return code;
+}
+
+int isIncorrectInput(char *input) {
+    if (strlen(input) != 4) {
+        printf("Wrong input!\n");
+        return 1;
+    } 
+    
+    for (int i = 0; i < 4; i++) {
+        if (input[i] < '0' || input[i] > '8') {
+            printf("Wrong input!\n");
+            return 1;
+        }
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            if (input[i] == input[j]) {
+                printf("Wrong input!\n");
+                return 1;
+            }
+        }
+    }
+    
+    return 0;
+}
+  
+int main(int argc, char **argv) {
+    int attempts = 10;
+    char *code = NULL;
+    
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-t") == 0) {
+            attempts = atoi(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "-c") == 0) {
+            code = argv[i + 1];
+        }
+    }
+    
+    if (code == NULL) {
+        code = generateRandomCode();
+    }
+    
+    printf("Will you find the secret code?\nPlease enter a valid guess:\n");
+    char input[15] = {0};
+    char c = 0;
+    int j = 0;
+    int flag = 1;
+    int correctPlacements, incorrectPlacements;
+    
+    for (int i = 0; i < attempts; i++) {
+        printf("---\nRound %d\n", i);
+        
+        do {
+            write(1, ">", 1);
+            j = 0;
+            
+            while ((flag = read(0, &c, 1)) > 0) {
+                if (c == '\n') {
+                    input[j] = '\0';
+                    break;
+                }
+                input[j] = c;
+                j++;
+            }
+            
+            if (flag == 0) {
+                return 0;
+            }
+        } while (isIncorrectInput(input));
+        
+        correctPlacements = 0;
+        incorrectPlacements = 0;
+        
+        for (int i = 0; i < 4; i++) {
+            if (code[i] == input[i]) {
+                correctPlacements++;
+            }
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (input[i] == code[j] && i != j) {
+                    incorrectPlacements++;
+                }
+            }
+        }
+        
+        if (correctPlacements == 4) {
+            printf("Congratulations! You did it!\n");
+            return 0;
+        } else {
+            printf("Correct placements: %d\n", correctPlacements);
+            printf("Incorrect placements: %d\n", incorrectPlacements);
+        }
+    }
+    
+    return 0;
+}

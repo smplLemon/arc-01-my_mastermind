3386d66ee241687091f343f7b923c76b58189196
Komiljonov_komronbek <157817727+assalomual@users.noreply.github.com>
2024-03-04 11:34:55 +0000

    komronbek_komiljonov_tugatdi

--- My_mastermaind.c
+++ My_mastermaind.c
@@ -1,8 +1,18 @@
-//ctrl + d ni oriniga ctrl + c ishni bajarayapti
+//ctrl + d ni orniga ctrl + c ishlidi
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+int compare_strings(char *str1, char *str2) {
+    while (*str1 && *str2) {
+        if (*str1 != *str2) {
+            return 0;
+        }
+        str1++;
+        str2++;
+    }
+    return (*str1 == '\0' && *str2 == '\0');
+}
 char *code_random_generate() {
     srand(time(NULL));
     char *code = (char *)malloc(sizeof(char) * 5);
@@ -66,13 +76,11 @@ char *receive_information() {
 int find_true_or_false_codes(char *code, char *guess) {
     int well = 0, missed = 0;
     int code_used = 0, input_used = 0;
-
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) well++;
         code_used |= (1 << (code[i] - '0'));
         input_used |= (1 << (guess[i] - '0'));
     }
-
     int common = code_used & input_used;
     for (int i = 0; i < 9; i++) {
         if (common & (1 << i))
@@ -95,7 +103,6 @@ void result_game(char *code, char *guess) {
 }
 void play_game(int attempts, char *code) {
     printf("Will you find the secret code?\nPlease enter a valid guess:\n");
-
     for (int i = 0; i < attempts; i++) {
         printf("---\nRound %d\n", i);
         char *guess = receive_information();
@@ -107,13 +114,12 @@ void play_game(int attempts, char *code) {
 int main(int argc, char **argv) {
     int attempts = 10;
     char *code = NULL;
-
     if (argc >= 3) {
         for (int i = 1; i < argc; i++) {
-            if (strcmp(argv[i], "-t") == 0) {
+            if (compare_strings(argv[i], "-t")) {
                 attempts = atoi(argv[i + 1]);
                 i++;    
-            } else if (strcmp(argv[i], "-c") == 0) {
+            } else if (compare_strings(argv[i], "-c")) {
                 code = argv[i + 1];
                 i++;
             }

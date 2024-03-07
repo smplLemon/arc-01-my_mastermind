c62d9303d41cf58ae66166682f7fc0e05a2ab931
rinkuo <157690181+rinkuo@users.noreply.github.com>
2024-03-07 19:15:14 +0500

    Update my_mastermind.c

--- my_mastermind.c
+++ my_mastermind.c
@@ -17,8 +17,9 @@ char *generate_code() {
 
 int valid_input(char *guess) {
     int length = 0;
-    for (; guess[length] != '\0'; ++length);
-
+    while (guess[length] != '\0') {
+        length++;
+    }
     if (length != 4) {
         return 0;
     }
@@ -26,6 +27,7 @@ int valid_input(char *guess) {
         if (guess[i] < '0' || guess[i] > '8') {
             return 0;
         }
+        
         for (int j = i + 1; j < 4; j++) {
             if (guess[i] == guess[j]) {
                 return 0;
@@ -35,9 +37,7 @@ int valid_input(char *guess) {
     return 1;
 }
 
-char *getin() {
-    write(1, ">", 1);
-    char *guess = (char *)malloc(100 * sizeof(char));
+void get_input(char *guess) {
     char c;
     int index = 0;
     int read_result;
@@ -48,25 +48,34 @@ char *getin() {
     guess[index] = '\0';
 
     if (read_result == 0) {
-        free(guess);
-        return NULL;
+        guess[0] = '\0';
     }
+}
 
+void validate_input(char *guess) {
     while (!valid_input(guess)) {
         printf("Wrong input!\n");
-        write(1, ">", 1);
-        index = 0;
-        while ((read_result = read(0, &c, 1)) > 0 && c != '\n') {
-            guess[index++] = c;
-        }
-        guess[index] = '\0';
-
-        if (read_result == 0) {
-            free(guess);
-            return NULL;
-        }
+        get_input(guess);
     }
-    return guess;
+}
+
+void getin(char *guess) {
+    write(1, ">", 1);
+    get_input(guess);
+    validate_input(guess);
+}
+
+void print_round(int round) {
+    printf("---\nRound %d\n", round);
+}
+
+void print_congratulations() {
+    printf("Congratz! You did it!\n");
+}
+
+void print_feedback(int well_placed, int misplaced) {
+    printf("Well placed pieces: %d\n", well_placed);
+    printf("Misplaced pieces: %d\n", misplaced);
 }
 
 void check_guess(char *code, char *guess, int *well_placed, int *misplaced) {
@@ -76,7 +85,6 @@ void check_guess(char *code, char *guess, int *well_placed, int *misplaced) {
     if (!valid_input(guess)) {
         return;
     }
-
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) {
             (*well_placed)++;
@@ -91,7 +99,44 @@ void check_guess(char *code, char *guess, int *well_placed, int *misplaced) {
     }
 }
 
-void arguments(int argc, char *argv[], char **code, int *attempts) {
+int handle_guess(char *code, char *guess, int attempts) {
+    int well_placed = 0;
+    int misplaced = 0;
+
+    check_guess(code, guess, &well_placed, &misplaced);
+
+    if (well_placed == 4) {
+        print_congratulations();
+        return 1;
+    } else {
+        print_feedback(well_placed, misplaced);
+        return 0;
+    }
+}
+
+void game_loop(char *code, int attempts) {
+    char *guess = (char *)malloc(100 * sizeof(char));
+    printf("Will you find the secret code?\n");
+    printf("Please enter a valid guess\n");
+    for (int i = 0; i < attempts; i++) {
+        print_round(i);
+        getin(guess);
+        if (guess[0] == '\0') {
+            free(code);
+            free(guess);
+            return;
+        }
+        if (handle_guess(code, guess, attempts)) {
+            free(guess);
+            free(code);
+            return;
+        }
+    }
+    free(guess);
+    free(code);
+}
+
+void handle_arguments(int argc, char *argv[], char **code, int *attempts) {
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-c") == 0 && i + 1 < argc) {
             *code = (char *)realloc(*code, 5 * sizeof(char));
@@ -108,43 +153,14 @@ void arguments(int argc, char *argv[], char **code, int *attempts) {
 int main(int argc, char *argv[]) {
     char *code = NULL;
     int attempts = 10;
-    int well_placed = 0;
-    int misplaced = 0;
-    char *guess = NULL;
 
-    arguments(argc, argv, &code, &attempts);
+    handle_arguments(argc, argv, &code, &attempts);
 
     if (code == NULL) {
         code = generate_code();
     }
 
-    printf("Will you find the secret code?\n");
-    printf("Please enter a valid guess\n");
+    game_loop(code, attempts);
 
-    for (int i = 0; i < attempts; i++) {
-        printf("---\nRound %d\n", i);
-        free(guess);
-        guess = getin();
-
-        if (guess == NULL) {
-            free(code);
-            return 0;
-        }
-
-        check_guess(code, guess, &well_placed, &misplaced);
-
-        if (well_placed == 4) {
-            printf("Congratz! You did it!");
-            free(code);
-            free(guess);
-            return 0;
-        } else {
-            printf("Well placed pieces: %d\n", well_placed);
-            printf("Misplaced pieces: %d\n", misplaced);
-        }
-    }
-
-    free(guess);
-    free(code);
     return 0;
 }

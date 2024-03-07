7020edbf4571182dc53863bdfaf00492494805f7
Mikeq128 <138781380+Mikeq128@users.noreply.github.com>
2024-03-07 21:20:43 +0500

    errno.c

--- errno.c
+++ errno.c
@@ -3,23 +3,52 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <unistd.h>
 #include "mastermind_h.h"
+#include "master_h.h"
+#define CD_LEN 4
 
-int rd_input(char* gues){
-    if (read(0, gues, 4 + 1) == -1){
-        printf("Error reading input.\n");
+int rd_input(char* guess){
+    int chars_read = 0;
+    char result;
+    int frst_rn = 1;
+    int count;
+    
+    while((count = read(STDIN_FILENO, &result, 1)) != -1){
+        if(frst_rn && !count){
+            exit(0);
+        }
+        if(frst_rn){
+            frst_rn = 0;
+        }
+        
+        if(result == '\n'){   
+            guess[chars_read] = '\0';
+            return 0;
+        } 
+        else if(chars_read >= CD_LEN){
+            while(read(STDIN_FILENO, &result, 1) && result != '\n');
+            return 1;
+        } 
+        else if(result < '0' || result > '9'){
+            if(result == '\n'){
+                exit(0);
+            }
+            while(read(STDIN_FILENO, &result, 1) && result != '\n');
+            return 1;
+        } 
+        guess[chars_read++] = result;
+    }
+    return 1;
+}
+
+int is_correct(int wlplcd){
+    if(wlplcd == 4){
+        printf("Congratz! You did it!\n");
         return 1;
     }
     return 0;
 }
-
-int is_correct(int well_placed){
-    if (well_placed == 4){
-        printf("Congratz! You guessed it right!\n");
-        return 1;
-    }
-    return 0;
-}
-void prnt_res(int well_placed, int misplaced){
-    printf("Well placed pieces: %d\nMisplaced pieces: %d\n", well_placed, misplaced);
+void prnt_res(int wlplcd, int misd){
+    printf("Well-placed pieces: %d\nMisplaced pieces: %d\n", wlplcd, misd);
 }

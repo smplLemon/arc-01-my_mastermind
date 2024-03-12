3a89f8042ef831daa79f8b59e57fbeaf85b72734
Burxonov Shukurullo <154730661+burxonovsh@users.noreply.github.com>
2024-03-12 14:53:03 +0500

    Create my_mastermind.c

---
+++ my_mastermind.c
@@ -0,0 +1,24 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+
+TARGET = my_mastermind
+SRCS = my_mastermind.c
+OBJS = $(SRCS:.c=.o)
+
+.PHONY: all clean fclean re
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+ $(CC) $(CFLAGS) $(OBJS) -o $(TARGET)
+
+%.o: %.c
+ $(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+ rm -f $(OBJS)
+
+fclean: clean
+ rm -f $(TARGET)
+
+re: fclean all

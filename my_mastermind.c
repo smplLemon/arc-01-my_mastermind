e03d3642b423b3a1eb793ea8a1e38087ed6d15a0
Istamovashakhzoda <istamovashahzoda@gmail.com>
2024-03-14 13:09:28 +0500

    finished

--- my_mastermind.c
+++ my_mastermind.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <unistd.h>
 
-int values(char *main_in) {
+int count_it(char *main_in) {
   int i = 0;
   while (main_in[i] != '\0') {
     i++;
@@ -21,7 +21,7 @@ char *strln(char *param_1, char *param_2) {
   return param_1;
 }
 
-int new_strchr(char *numbers, char num) {
+int find_number_in(char *numbers, char num) {
   for (int i = 0; numbers[i] != '\0'; i++) {
     if (numbers[i] == num) {
       return i;
@@ -30,15 +30,15 @@ int new_strchr(char *numbers, char num) {
   return -1;
 }
 
-int new_input(char param_1) {
+int is_in_correct_interval(char param_1) {
   if (param_1 >= '0' && param_1 <= '8') {
     return 1;
   }
   return 0;
 }
 
-int bubble(char *main_in) {
-  int last = values(main_in);
+int are_there_similar(char *main_in) {
+  int last = count_it(main_in);
   for (int i = 0; i < last - 1; i++) {
     for (int j = i + 1; j < last; j++) {
       if (main_in[i] == main_in[j]) {
@@ -49,7 +49,7 @@ int bubble(char *main_in) {
   return 0;
 }
 
-int nested(char *main_in, char *numbeses) {
+int compare_the_numbers(char *main_in, char *numbeses) {
   int b = 0;
   int c = 0;
   for (int i = 0; main_in[i] != '\0'; i++) {
@@ -70,7 +70,7 @@ int nested(char *main_in, char *numbeses) {
   return 0;
 }
 
-char *inrct() {
+char *try_to_read() {
   int i = 0;
   char j;
   char *inclusion = malloc(6);
@@ -92,7 +92,7 @@ char *numbers() {
   char num;
   while (i < 4) {
     num = rand() % 9 + 48;
-    if (new_strchr(numbers, num) == -1) {
+    if (find_number_in(numbers, num) == -1) {
       numbers[i] = num;
       i++;
     }
@@ -101,14 +101,14 @@ char *numbers() {
 }
 
 int error(char *main_in) {
-  int len = values(main_in);
+  int len = count_it(main_in);
   int i = 0;
-  if ((len != 4) || (bubble(main_in) == 1)) {
+  if ((len != 4) || (are_there_similar(main_in) == 1)) {
     printf("Wrong input!\n");
     return 1;
   }
   while (i < len) {
-    if ((new_input(main_in[i]) == 0)) {
+    if ((is_in_correct_interval(main_in[i]) == 0)) {
       printf("Wrong input!\n");
       return 1;
     }
@@ -117,7 +117,7 @@ int error(char *main_in) {
   return 0;
 }
 
-void begin(int round, char *numbeses) {
+int starter(int round, char *numbeses) {
   printf("Will you find the secret code?\n");
   printf("Please enter a valid guess\n");
   char *input_value;
@@ -126,25 +126,25 @@ void begin(int round, char *numbeses) {
   while (i < round) {
     printf("---\n");
     printf("Round %d\n", i);
-    if (strcmp(input_value = inrct(), "stop") == 0)
-      return;
-    flag = error(input_value);
+    do{
+        if (strcmp(input_value = try_to_read(), "stop") == 0)
+        return 0;
+    }while((flag = error(input_value)) == 1);
+  
     if (flag == 0) {
-      if (1 == nested(input_value, numbeses)) {
-        return;
+      if (1 == compare_the_numbers(input_value, numbeses)) {
+        return 0;
       }
       i++;
     }
   }
 
-  if (flag == 0)
-  {
-    exit(0);
-  }
-  
+  if (flag == 0) return 1;
+
+  return 0;
 }
 
-void replace(int argc, char **argv) {
+void command_line_supervisor(int argc, char **argv) {
   int i = 1;
   int round = 10;
   char *numbeses = numbers();
@@ -157,10 +157,10 @@ void replace(int argc, char **argv) {
       i += 2;
     }
   }
-  begin(round, numbeses);
+  if(starter(round, numbeses)) return;
 }
 
 int main(int argc, char **argv) {
-  replace(argc, argv);
+  command_line_supervisor(argc, argv);
   return 0;
 }

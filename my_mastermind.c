c734251ee18d63eb38dcd068164aa42ee039bc20
muhammadnurullayev <153077985+muhammadnurullayev@users.noreply.github.com>
2024-03-20 00:14:10 +0500

    the range of functions has been shortened

--- my_mastermind.c
+++ my_mastermind.c
@@ -12,39 +12,42 @@ void mystrcpy(char *, char *);
 void readinput(char *);
 int count_wellplaced(char *, char *);
 int count_misplaced(char *, char *);
+int check_code(char *);
 
 void mystrcpy(char *s1, char *s2){
     while((*s1++ = *s2++));
 }
 
-void readinput(char *buf){
+void reader(char *buf, flag *moon, int *i, ssize_t *bytes_read, char *c) {
+    while (((*bytes_read = read(0, c, 1)) > 0) && (*c != '\n')) {
+        if (*c >= '0' && *c <= '8' && *i < CODELEN) {
+            if (*i > 0) {
+                for (int j = 0; j < *i; j++) {
+                    if (*(buf + j) == *c) {
+                        *moon = 1;
+                        break;
+                    }
+                }
+            }
+            if (!(*moon)) 
+                *(buf + (*i)++) = *c;
+            else 
+                break;
+        } else {
+            *moon = 1;
+            break;
+        }
+    }
+}
+
+void readinput(char *buf) {
     flag moon;
     int i;
     char c;
     ssize_t bytes_read;
-
     do {
         moon = 0, i = 0;
-        while (((bytes_read = read(0, &c, 1)) > 0) && (c != '\n')) {
-            if (c >= '0' && c <= '8' && i < CODELEN) {
-                if (i > 0) {
-                    for (int j = 0; j < i; j++) {
-                        if (*(buf + j) == c) {
-                            moon = 1;
-                            break;
-                        }
-                    }
-                }
-                if (!moon) {
-                    *(buf + i++) = c;
-                } else {
-                    break;
-                }
-            } else {
-                moon = 1;
-                break;
-            }
-        }
+        reader(buf, &moon, &i, &bytes_read, &c);
         if (moon || i != CODELEN) {
             if (bytes_read == 0) {
                 eof_encountered = 1;
@@ -59,7 +62,7 @@ void readinput(char *buf){
     } while (1);
 }
 
-void generate_unique_code(char *codeval) {
+void generate_unique_code(char *codeval) { //10
     int i, j;
     char digits[] = "012345678";
     srand((unsigned)time(NULL));
@@ -71,7 +74,7 @@ void generate_unique_code(char *codeval) {
     codeval[CODELEN] = '\0';
 }
 
-int count_wellplaced(char *guess, char *codeval){
+int count_wellplaced(char *guess, char *codeval){ //9
     int wellplaced, i;
     for(wellplaced = 0, i = 0; i < CODELEN; i++, guess++, codeval++)
         if(*guess == *codeval){
@@ -94,54 +97,64 @@ int count_misplaced(char *guess, char *codeval){
     return misplaced;
 }
 
-int main(int argc, char **argv){
-    if(!(argc % 2))return 1;
-    int tries = 10, i = 0;
-    char codeval[CODELEN + 1] = {};
-
-    while(--argc){
-        if(**++argv == '-'){
-            argc--;
-            switch(*++*argv){
-                case 'c':
-                    mystrcpy(codeval, *++argv);
-                    for(i = 0; codeval[i] && i < CODELEN; i++)
-                        if(!(codeval[i] >= '0' && codeval[i] <= '8')) return 1;
-                    if (i < CODELEN || codeval[i]) return 1;
-                    for (i = 0; i < CODELEN; i++) {
-                        for (int j = i + 1; j < CODELEN; j++) {
-                            if (codeval[i] == codeval[j]) return 1;
-                        }
-                    }
-                    break;
-                case 't':
-                    for(i = 0, ++argv; *(*argv + i); i++)
-                        if(!(*(*argv + i) >= '0' && *(*argv + i) <= '9')) return 1;
-                    if((tries = atoi(*argv)) == 0) return 1;
-                    break;
-                }
-            }
-    }
-
+int second_main(char* codeval, int tries){
     char guess[CODELEN + 1];
-    int misplaced, wellplaced, k = 0;
-
+    int misplaced, wellplaced;
     if (!codeval[0]) generate_unique_code(codeval);
     printf("Will you find the secret code?\nPlease enter a valid guess\n");
-    do {
-        if (eof_encountered)
-            break;
-        printf("---\nRound %d\n", k);
+    for (int i = 1; i <= tries; ++i){
+        if (eof_encountered) break;
+        printf("---\nRound %d\n", i);
         for (int j = 0; j < CODELEN; j++) matched[j] = 0;
         readinput(guess);
         wellplaced = count_wellplaced(guess, codeval);
         misplaced = count_misplaced(guess, codeval);
         if(wellplaced == CODELEN)break;
-        if (!eof_encountered) {
-            printf("Well placed pieces: %d\nMisplaced pieces: %d\n", wellplaced, misplaced);
-        }
-        if(++k >= tries) break;
-    } while(1);
+        if (!eof_encountered) printf("Well placed pieces: %d\nMisplaced pieces: %d\n", wellplaced, misplaced);
+    }
     if (wellplaced == CODELEN) printf("Congratz! You did it!\n");
+    return 0;
+}
+
+int check_code(char* codeval) {
+    int i, j;
+    for(i = 0; codeval[i] && i < CODELEN; i++)
+        if(!(codeval[i] >= '0' && codeval[i] <= '8')) return 1;
+    if (i < CODELEN || codeval[i]) return 1;
+    for (i = 0; i < CODELEN; i++) {
+        for (j = i + 1; j < CODELEN; j++) {
+            if (codeval[i] == codeval[j]) return 1;
+        }
+    }
+    return 0;
+}
+
+int check_users_input(int argc, char** argv, char* codeval, int* tries) {
+    while(argc > 1) {
+        if(**argv == '-'){
+            switch(*++*argv){
+                case 'c':
+                    mystrcpy(codeval, *++argv);
+                    if (check_code(codeval)) return 1;
+                    argc--;
+                    break;
+                case 't':
+                    *tries = atoi(*++argv);
+                    argc--;
+                    break;
+            }
+        }
+        argv++, argc--;
+    }
+    return 0;
+}
+
+int main(int argc, char **argv){
+    if(!(argc % 2)) return 1;
+    int tries = 10;
+    char codeval[CODELEN + 1] = {};
+    int result = check_users_input(argc, argv, codeval, &tries);
+    if (result == 1) return 1;
+    second_main(codeval, tries);
     return 0;
 }

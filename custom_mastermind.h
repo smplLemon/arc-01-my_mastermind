1f80c4e7daf23a27613d035cfe66b1f8607d036d
YulduzAskarova <151628405+YulduzAskarova@users.noreply.github.com>
2024-03-20 10:56:39 +0500

    Fixed Version

---
+++ custom_mastermind.h
@@ -0,0 +1,15 @@
+// custom_mastermind.h
+#ifndef MYSTERIOUS_H
+#define MYSTERIOUS_H 
+
+#define LENGTH_NUMBER 4
+void authenticate_decryption(char decryption[]);
+int perplexing_strlen(char *string);
+int enigmatic_scanf(char *string);
+void cryptic_strcpy(char *destination, char *source);
+void generate_hidden_code(char hidden_code[], char *encoded);
+void authenticate_decryption(char decryption[]);
+void get_decryption(char decryption[]);
+void compare_codes(char hidden_code[], char decryption[], int *accuratelyPlaced, int *inaccuratelyPlaced);
+
+#endif

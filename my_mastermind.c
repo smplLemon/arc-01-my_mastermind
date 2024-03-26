430ade3c36d4b4992cb3e0f1d46d5376bc027fb1
IldizovNodirbek <158564155+IldizovNodirbek@users.noreply.github.com>
2024-03-26 08:46:30 +0300

    corrected again my_mastermind

--- my_mastermind.c
+++ my_mastermind.c
@@ -9,7 +9,6 @@
 char* generate_secret_code() {
     srand(time(NULL));
     char *secret_code = (char *)malloc(sizeof(char) * (CODE_LENGTH + 1));
-    
     for(int i = 0; i < CODE_LENGTH; i++) {
         secret_code[i] = rand() % 9 + '0';
         for(int j = 0; j < i; j++) {    
@@ -20,7 +19,6 @@ char* generate_secret_code() {
         }
     }
     secret_code[CODE_LENGTH] = '\0';
-    
     return secret_code;
 }
 
@@ -32,24 +30,28 @@ void print_round(int round) {
     printf("---\nRound %d\n", round);
 }
 
-int is_valid_input(const char *input) {
+int validate_input_length(const char *input) {
     int input_length = 0;
     while (input[input_length] != '\0') {
         input_length++;
     }
-
     if (input_length != CODE_LENGTH) {
         printf("Wrong input!\n");
         return 0;
     }
+    return 1;
+}
 
+int is_valid_input(const char *input) {
+    if (!validate_input_length(input)) {
+        return 0;
+    }
     for (int i = 0; i < CODE_LENGTH; i++) {
         if (input[i] < '0' || input[i] > '8') {
             printf("Wrong input!\n");
             return 0;
         }
     }
-
     for (int i = 0; i < CODE_LENGTH; i++) {
         for (int j = i + 1; j < CODE_LENGTH; j++) {
             if (input[i] == input[j]) {
@@ -58,20 +60,21 @@ int is_valid_input(const char *input) {
             }
         }
     }
-
     return 1;
 }
 
-int check_guess(char *secret_code, char *guess) {
+int count_well_placed(char *secret_code, char *guess) {
     int well_placed = 0;
-    int miss_placed = 0;
-    
     for(int i = 0; i < CODE_LENGTH; i++) {
         if(secret_code[i] == guess[i]) {
             well_placed++;
         }
     }
-    
+    return well_placed;
+}
+
+int count_miss_placed(char *secret_code, char *guess) {
+    int miss_placed = 0;
     for(int i = 0; i < CODE_LENGTH; i++) {
         for(int j = 0; j < CODE_LENGTH; j++) {
             if(i != j && guess[i] == secret_code[j]) {
@@ -79,6 +82,12 @@ int check_guess(char *secret_code, char *guess) {
             }
         }
     }
+    return miss_placed;
+}
+
+int check_guess(char *secret_code, char *guess) {
+    int well_placed = count_well_placed(secret_code, guess);
+    int miss_placed = count_miss_placed(secret_code, guess);
     
     if(well_placed == CODE_LENGTH) {
         printf("Congratz! You did it!\n");
@@ -89,59 +98,59 @@ int check_guess(char *secret_code, char *guess) {
     return 0;
 }
 
-int main(int argc, char **argv) {
-    char *secret_code = NULL;
-    int max_attempts = 10;
-   
+int get_user_input(char *input) {
+    char input_index = 0;
+    int input_complete = 0;
+    int read_status = 1;
+    while(1) {
+        write(1, ">", 1);
+        input_complete = 0;
+        while((read_status = read(0, &input_index, 1)) > 0) {
+            if(input_index == '\n') {
+                input[input_complete] = '\0';
+                break;
+            }
+            input[input_complete] = input_index;
+            input_complete++;
+        }
+        if(read_status == 0) return 1;
+        if(is_valid_input(input)) break;
+    }
+    return 0;
+}
+
+void handle_round(char *secret_code, int max_attempts) {
+    char input[CODE_LENGTH + 1] = {0};
+    for(int i = 0; i < max_attempts; i++) {
+        print_round(i);
+        get_user_input(input);
+        if(check_guess(secret_code, input)) {
+            break;
+        }
+    }
+}
+
+void process_game_options(int argc, char **argv, char **secret_code, int *max_attempts) {
     for(int i = 0; i < argc; i++) {
         switch (argv[i][1]) {
             case 't':
-                max_attempts = atoi(argv[i+1]);
+                *max_attempts = atoi(argv[i+1]);
                 break;
             case 'c':
-                secret_code = argv[i+1];
+                *secret_code = argv[i+1];
                 break;
             default:
                 break;
         }
     }
-    
-    if(secret_code == NULL) secret_code = generate_secret_code();
-    
-    print_welcome_message();
-    
-    char input[20] = {0};
-    char input_index = 0;
-    int input_complete = 0;
-    int read_status = 1;
-    
-    for(int i = 0; i < max_attempts; i++) {
-        print_round(i);
-        while(1) {
-            write(1, ">", 1);
-            input_complete = 0;
-            while((read_status = read(0, &input_index, 1)) > 0) {
-                if(input_index == '\n') {
-                    input[input_complete] = '\0';
-                    break;
-                }
-                input[input_complete] = input_index;
-                input_complete++;
-            }
-           
-            if(read_status == 0) {
-                return 0;
-            }
-            if(is_valid_input(input)) {
-                break;
-            }
-        }
-            if(check_guess(secret_code, input)) {
-                break;
-            }
-    }
-    
-    return 0;
 }
 
-
+int main(int argc, char **argv) {
+    char *secret_code = NULL;
+    int max_attempts = 10;
+    process_game_options(argc, argv, &secret_code, &max_attempts);
+    if(secret_code == NULL) secret_code = generate_secret_code();
+    print_welcome_message();
+    handle_round(secret_code, max_attempts);
+    return 0;
+}

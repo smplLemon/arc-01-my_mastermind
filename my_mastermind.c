9c6ab812f98d7cdee363b13cd8f5d1752b62ede7
YulduzAskarova <151628405+YulduzAskarova@users.noreply.github.com>
2024-03-30 09:09:12 +0500

    Update my_mastermind.c

--- my_mastermind.c
+++ my_mastermind.c
@@ -1,14 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
-#include "custom_mastermind.h"
+#include <time.h>
+#include <unistd.h>
+
+int perplexing_strlen(char *string){
+    int length = 0;
+    while(string[length] != '\0'){
+        length++;
+    }
+    return length;
+}
+
+int enigmatic_scanf(char *string){
+    int index = 0;
+    char letter;
+    write(1, ">", 1);
+    while(read(STDIN_FILENO, &letter, 1) > 0){
+        if(letter == '\n' || index == 4 + 1){
+            string[index] = '\0';
+            return 1;
+        }
+        string[index++] = letter;
+    }
+    if(index == 0){
+        exit(0);
+    }
+    return 0;
+}
+
+void cryptic_strcpy(char *destination, char *source){
+    while(*source != '\0'){
+        *destination = *source;
+        destination++;
+        source++;
+    }
+    *destination = '\0';
+}
+
+void generate_hidden_code(char hidden_code[], char *encoded){
+    if(encoded != NULL && perplexing_strlen(encoded) == 4){
+        cryptic_strcpy(hidden_code, encoded);
+    } else{
+        int used_digits[9] = {0}; 
+        srand(time(NULL));
+        for(int i = 0; i < 4; i++){
+            int digit;
+            digit = rand() % 9;
+            while(used_digits[digit]){
+                digit = rand() % 9;
+            }
+            used_digits[digit] = 1;
+            hidden_code[i] = digit + '0';
+        }
+    }
+}
+
+void authenticate_decryption(char decryption[]) {
+    int visited[10] = {0}; 
+    int invalid_guess = 0; 
+    if (perplexing_strlen(decryption) != 4) {
+        invalid_guess = 1;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        if (decryption[i] == '9' || decryption[i] < '0' || decryption[i] > '8') {
+            invalid_guess = 1;
+            break;
+        }
+        if (visited[decryption[i] - '0']) {
+            invalid_guess = 1;
+            break;
+        }
+        visited[decryption[i] - '0'] = 1;
+    }
+
+    if (invalid_guess) {
+        printf("Wrong input\n");
+        enigmatic_scanf(decryption); 
+        authenticate_decryption(decryption);
+    }
+}
+
+void get_decryption(char decryption[]){
+    enigmatic_scanf(decryption);
+    authenticate_decryption(decryption);
+}
+
+void compare_codes(char hidden_code[], char decryption[], int *accuratelyPlaced, int *inaccuratelyPlaced){
+    *accuratelyPlaced = 0;
+    *inaccuratelyPlaced = 0;
+
+    for(int i = 0; i < 4; i++){
+        if(decryption[i] == hidden_code[i]){
+           (*accuratelyPlaced)++;
+        } else{
+            for(int j = 0; j < 4; j++){
+                if(i != j && decryption[i] == hidden_code[j]){
+                   (*inaccuratelyPlaced)++;
+                    break;
+                }
+            }
+        }
+    }
+}
 
 int main(int argument_count, char *arguments[]){
-    char cryptic_code[LENGTH_NUMBER];
+    char cryptic_code[4];
     char *encoded = NULL;
     int trials = 10;
     int accuratelyPlaced, inaccuratelyPlaced;
-    char decryption[LENGTH_NUMBER + 1];
+    char decryption[5];
     int counter = 0;
     int cd_gused = 0; 
 
@@ -27,7 +129,7 @@ int main(int argument_count, char *arguments[]){
         printf("Round %d\n", counter++);
         get_decryption(decryption);
         compare_codes(cryptic_code, decryption, &accuratelyPlaced, &inaccuratelyPlaced);
-        if(accuratelyPlaced == LENGTH_NUMBER){
+        if(accuratelyPlaced == 4){
             printf("Congratz! You did it!\n");
             cd_gused = 1; 
             break;
